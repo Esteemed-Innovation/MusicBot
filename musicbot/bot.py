1b6743ff4a5cfb4bb1ba552c55312b43aedf339a
elifoster <elifosterwy@gmail.com>
2017-03-10 01:32:54 -0800

    Prevent adding duplicates to autoplaylist

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -382,10 +382,10 @@ class MusicBot(discord.Client):
         channel = entry.meta.get('channel', None)
         author = entry.meta.get('author', None)
 
-
-        self.autoplaylist.append(entry.url)
-        write_file(self.config.auto_playlist_file, self.autoplaylist)
-        print("[Info] Added %s to autoplaylist" % entry.url)
+        if entry.url not in self.autoplaylist:
+            self.autoplaylist.append(entry.url)
+            write_file(self.config.auto_playlist_file, self.autoplaylist)
+            print("[Info] Added %s to autoplaylist" % entry.url)
 
         if channel and author:
             last_np_msg = self.server_specific_data[channel.server]['last_np_msg']
